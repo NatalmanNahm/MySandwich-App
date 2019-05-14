22a5e21b91bdcc56de9c058a974b92ea2984b196
Natalman <sperobb@outlook.com>
2019-05-13 20:19:08 -0500

    modified activity_detail.xml and rebuild the project

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,20 @@
 package com.udacity.sandwichclub.utils;
 
+import android.content.Context;
+
 import com.udacity.sandwichclub.model.Sandwich;
 
 public class JsonUtils {
 
+    /**
+     * This method accepts a string of json data then returns a string array
+     * data about sandwich that will be used to populate data on the UI.
+     * @param json
+     * @return Sandwich data to be displayed
+     */
     public static Sandwich parseSandwichJson(String json) {
+        //Get the json data to position with the elements in the activity_detail.xml
+
         return null;
     }
 }
